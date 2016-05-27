63598288795101032d16a681a1a57cce2b538a44
Angel Bermeo <bermeom@javeriana.edu.co>
2016-05-27 00:30:03 -0500

    Update files

--- LQRD.m
+++
@@ -1,6 +0,0 @@
-function K=LQRD(A,B,Q,R,N)
-Pk(:,:,N)=Q;
-for i= N:-1:2
-    Pk(:,:,i-1)=A'*(Pk(:,:,i)-Pk(:,:,i)*B*((B'*Pk(:,:,i)*B+R)^-1)*B'*Pk(:,:,i))*A+Q;
-    K(:,i-1)=((B'*Pk(:,:,i)*B+R)^-1*B'*Pk(:,:,i)*A);
-end
